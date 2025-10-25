c4447d1034d00ce139445818a424e0bf85725516
Juan Pedro Michelini Lopez <juanpemichelini@gmail.com>
2025-10-25 13:11:09 -0300

    feat(GestionarReserva): TDD GREEN - reservation service

---
+++ Obligatorio/Material/Codigo/Frontend/src/app/services/reservation.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { CommonService } from './CommonService';
+import { StorageManager } from '../utils/storage-manager';
+
+@Injectable({ providedIn: 'root' })
+export class ReservationService {
+
+  private url = environment.apiUrl + '/api/reservation';
+
+  constructor(
+    private http: HttpClient,
+    private commonService: CommonService,
+    private storageManager: StorageManager) { }
+
+  getHttpHeaders(): HttpHeaders {
+    let login = JSON.parse(this.storageManager.getLogin());
+    let token = login ? login.token : "";
+
+    return new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token);
+  }
+
+  /** POST: Consult reservations by user */
+  getReservations(email: string, secret: string): Observable<any[]> {
+    return this.http.post<any[]>(`${this.url}/consult`, { email, secret }, { headers: this.getHttpHeaders() })
+      .pipe(
+        tap(),
+        catchError(this.handleError<any[]>('Get Reservations', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.log(`${operation} failed: ${error.error?.message || error.message}`);
+      return of(result as T);
+    };
+  }
+
+  /** Log a ReservationService error with the MessageService */
+  private log(message: string) {
+    this.commonService.updateToastData(message, "danger", "Error");
+  }
+}
