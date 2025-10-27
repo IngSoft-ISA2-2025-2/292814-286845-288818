e0f4d40dae404cf279eec5cfb08e2bdf790fd367
chrisingrey <142596158+chrisingrey@users.noreply.github.com>
2025-10-27 09:15:30 -0300

    chore(pipeline-configuration): fix frontend-e2e-cypress + fix cypress.config

--- Obligatorio/Material/Codigo/Frontend/cypress.config.ts
+++ Obligatorio/Material/Codigo/Frontend/cypress.config.ts
@@ -1,22 +1,30 @@
-const { defineConfig } = require("cypress");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200",
-    specPattern: "cypress/e2e/**/*.feature",
-    supportFile: false,
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
+
       on(
-        "file:preprocessor",
+        'file:preprocessor',
         createBundler({
-          plugins: [createEsbuildPlugin(config)],
+          plugins: [
+            createEsbuildPlugin(config),
+            require('node-stdlib-browser/helpers/esbuild/plugin')(require('node-stdlib-browser'))
+          ],
+          define: {
+            'process.env.NODE_ENV': JSON.stringify(process.env['NODE_ENV'] || 'test'),
+          },
+          inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
         })
       );
+
       return config;
     },
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'http://localhost:4200',
   },
 });
